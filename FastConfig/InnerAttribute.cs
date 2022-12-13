16e5935ae152edfe46e374f05697f909b82bb292
kate <kate@dariox.club>
2022-12-13 15:19:21 +0800

    Comments

--- FastConfig/InnerAttribute.cs
+++ FastConfig/InnerAttribute.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace FastConfig
 {
+    /// <summary>
+    /// The class that this attribute is assigned to must have a constructor that takes no arguments.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class InnerAttribute : Attribute
     {
--- FastConfigExample/Program.cs
+++ FastConfigExample/Program.cs
@@ -4,11 +4,14 @@ namespace FastConfigExample
 {
     public class Config
     {
+        // Indicate that this field belongs in the "General" group
         [Group("General")]
         public bool Enable { get; set; }
-        public AuthConfig Authentication { get; set; } = new();
+        public AuthConfig Authentication { get; set; }
     }
+    // Define that this is a nested item and should be searched
     [Inner]
+    // Default all fields and properties to the "Authentication" group
     [Group("Authentication")]
     public class AuthConfig
     {
@@ -16,17 +19,29 @@ namespace FastConfigExample
         public string Token { get; set; }
         public bool Remember { get; set; }
     }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            var fastConfig = new FastConfigSource<Config>(@"example.ini");
-            var parsed = fastConfig.Parse();
-            var dict = fastConfig.ToDictionary(parsed);
+            // Load from "example.ini"
+            FastConfigSource<Config> fastConfig = new FastConfigSource<Config>(@"example.ini");
 
-            PrintContent(fastConfig, parsed, dict);
+            // Deserialize file content to instance of Config.
+            Config parsed = fastConfig
+                .Parse();
+
+            // Serialize instance of Config to a dictionary.
+            Dictionary<string, Dictionary<string, object>> dict = fastConfig
+                .ToDictionary(parsed);
+
+            // Create Ini file content from the deserialized
+            // class, or any class that is the same type.
+            string[] fileLines = fastConfig.ToFileLines(parsed);
+
+            PrintContent(parsed, dict, fileLines);
         }
-        static void PrintContent(FastConfigSource<Config> fastConfig, Config parsed, Dictionary<string, Dictionary<string, object>> dict)
+        static void PrintContent(Config parsed, Dictionary<string, Dictionary<string, object>> dict, string[] fileLines)
         {
             var serializer = new System.Text.Json.JsonSerializerOptions()
             {
@@ -42,8 +57,7 @@ namespace FastConfigExample
             Console.WriteLine($"================================ To Dictionary");
             Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(dict, serializer));
             Console.WriteLine($"================================ Output Content");
-            Console.WriteLine(string.Join("\n", fastConfig.ToFileLines(parsed)));
-            Console.WriteLine("\n\n\n\n\n\n\n\n");
+            Console.WriteLine(string.Join("\n", fileLines));
         }
     }
 }
